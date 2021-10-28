5b5c5d30e6146212f4e7011197c71f4f7a5d7d92
101010sun <D0782838@mail.fcu.edu.tw>
2021-10-28 22:44:46 +0800

    新增註解

---
+++ getData.py
@@ -0,0 +1,111 @@
+from pymongo import MongoClient
+import Wallet
+import gridfs
+import cv2
+
+conn = MongoClient()
+# database
+db = conn.localcurrency
+# collection
+col_Information_user         = db.Information_user
+col_Information_demand       = db.Information_demand
+col_Photo                    = db.Photo
+col_Check_community_manager  = db.Check_community_manager
+col_Check_community_user     = db.Check_community_user
+col_Check_createcommunity    = db.Check_createcommunity
+col_Communitymembers         = db.Communitymembers
+col_Community_bulletin       = db.Community_bulletin
+col_System_bulletin          = db.System_bulletin
+# connect error or not
+col_Information_user.stats
+col_Information_demand.stats
+col_Photo.stats
+col_Check_community_manager.stats
+col_Check_community_user.stats
+col_Check_createcommunity.stats
+col_Communitymembers.stats
+col_Community_bulletin.stats
+col_System_bulletin.stats
+
+# 檢查_此信箱和電話是否被使用過
+def Check_userinfor(email,phone):
+  cursor = col_Information_user.find({"email":str(email)})
+  data = [d for d in cursor]
+  cursor2 = col_Information_user.find({"phone":str(phone)})
+  data2 = [d for d in cursor2]
+  if data == list([]) and data2 == list([]):
+    return True
+  else:
+    return False
+
+# 檢查_此身分證號碼是否被使用過
+def Check_id(id_card):
+  cursor = col_Information_user.find({"id_card":str(id_card)})
+  data = [d for d in cursor]
+  if data == list([]): 
+    return True
+  else:
+    return False
+
+# 檢查_此帳號是否被使用過
+def Check_account(account):
+  cursor = col_Information_user.find({"account":str(account)})
+  data = [d for d in cursor]
+  if data == list([]): 
+    return True
+  else:
+    return False
+
+# 取得_此帳號的加密密碼
+def Taken_password(account):
+    projectionFields = ['e_password']
+    cursor = col_Information_user.find({"account": str(account)}, projection = projectionFields)
+    data = [d for d in cursor]
+    if data != list([]):
+        return data[0]['e_password']
+    else:
+        return None
+
+# 取得_此帳號的基本資料
+def Taken_userinfo(account):
+    myquery = {'account': account}
+    projectionFields = ['account']
+    cursor = col_Information_user.find(myquery)
+    data = [d for d in cursor]
+    if data != list([]):
+        
+        return data
+    else:
+        return None
+
+# 取得_此帳號的私鑰
+def Taken_privatekey(account,password):
+  projectionFields = ['private_key']
+  cursor = col_Information_user.find({"account": str(account)}, projection = projectionFields)
+  data = [d for d in cursor]
+  e_private_key = data[0]['private_key']
+  privatekey = Wallet.decryption_privatekey(e_private_key, password)
+  if data != list([]):
+    return privatekey
+  else:
+    return None
+
+# 取得_照片檔案
+def download_photo(name):
+    data = db.fs.files.find_one({'filename':name})
+    my_id = data['_id']
+    fs = gridfs.GridFS(db) # fs--選取資料庫
+    outputdata = fs.get(my_id).read()
+    download_location = "C:/Users/USER/block_chain/Pic/" + name
+    output = open(download_location, "wb")
+    output.write(outputdata)
+    output.close()
+    print("download complete")
+    
+    img = cv2.imread(name) # 圖片顯示
+    cv2.imshow('My Profile', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# ----test----
+# Taken_userinfo('10')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, json, request , jsonify
 from flask_cors import CORS
-import Wallet, Database
+import Wallet, insertData, getData
 
 app = Flask(__name__)
 CORS(app)
 
 # 使用者登入路由
+# [user_account, user_password, user_id]
 @app.route('/login', methods=['POST'])
 def login():
     insertValues = request.get_json()
@@ -13,13 +14,14 @@ def login():
     user_password = insertValues['user_password']
     user_id = insertValues['user_id']
     verify_epass = Wallet.encryption_password(user_password, user_id)
-    user_epass = Database.Taken_password(user_account)
+    user_epass = getData.Taken_password(user_account)
     if verify_epass == user_epass:
         return jsonify({'result': 'success'})
     else:
         return jsonify({'result': 'fail', 'reason': '帳號密碼錯誤'})
 
 # 使用者註冊
+# [user_name, user_id, user_sex, user_birth, user_email, user_phone, user_address, user_account, user_password]
 @app.route('/signup', methods=['POST'])
 def signup():
     insertValues = request.get_json()
@@ -32,39 +34,56 @@ def signup():
     user_address = insertValues['user_address']
     user_photo = '' #--?
     e_id = Wallet.encryption_id_card(user_id)
-    check_id = Database.Check_account(e_id)
+    check_id = getData.Check_id(e_id)
     if check_id:
         user_account = insertValues['user_account']
         user_password = insertValues['user_password']
-        walletaddress, private_key = Wallet.generate_address()
-        public_key = walletaddress
-        e_password = Wallet.encryption_password(user_password, user_id) # 加密密碼 (明文身分證)
-        e_private_key = Wallet.encryption_privatekey(private_key, user_password) # 加密私鑰 (明文密碼)
-        Database.insert_Information_user(user_name, user_sex, e_id, user_birth, user_email, user_phone, user_address, user_account, user_photo, walletaddress, public_key, e_private_key, e_password)
-        return jsonify({'result': 'success'})
+        check_account = getData.Check_account(user_account)
+        if check_account:
+            walletaddress, private_key = Wallet.generate_address()
+            public_key = walletaddress
+            e_password = Wallet.encryption_password(user_password, user_id) # 加密密碼 (明文身分證)
+            e_private_key = Wallet.encryption_privatekey(private_key, user_password) # 加密私鑰 (明文密碼)
+            insertData.insert_Information_user(user_name, user_sex, e_id, user_birth, user_email, user_phone, user_address, user_account, user_photo, walletaddress, public_key, e_private_key, e_password)
+            return jsonify({'result': 'success'})
+        else:
+            return jsonify({'result': 'fail', 'reason': '帳號已註冊過'})
     else:
         return jsonify({'result': 'fail', 'reason': '身分證號碼已註冊過'})
 
-# 使用者註冊 驗證身分證
+# 使用者註冊、驗證身分證
+# [user_id]
 @app.route('/signup/verify_id', methods=['POST'])
 def verifyid():
     insertValues = request.get_json()
     user_id = insertValues['user_id']
     e_id = Wallet.encryption_id_card(user_id)
-    check_id = Database.Check_account(e_id)
+    check_id = getData.Check_account(e_id)
     if check_id:
         return jsonify({'result': 'success'})
     else:
         return jsonify({'result': 'fail', 'reason': '身分證號碼已註冊過'})
 
-# 取社區公告
-@app.route('/get-new', methods=['POST'])
-def getnew():
+# 新增社區公告
+# [communityname, bulletin_title, bulletin_context]
+@app.route('/create-new/community', methods=['POST'])
+def createComNew():
     insertValues = request.get_json()
-    user_account = insertValues['user_account']
-    #- 去資料庫取這個使用者所在的所有社區
-    #- 取所有社區的公告
-    #- 取系統公告
+    com_name = insertValues['communityname']
+    bul_title = insertValues['bulletin_title']
+    bul_context = insertValues['bulletin_context']
+    insertData.insert_Community_bulletin(com_name, bul_title, bul_context)
+    return jsonify({'result': 'success'})
+
+# 新增系統公告
+# [bulletin_title, bulletin_context]
+@app.route('/create-new/system', methods=['POST'])
+def createSysNew():
+    insertValues = request.get_json()
+    bul_title = insertValues['bulletin_title']
+    bul_context = insertValues['bulletin_context']
+    insertData.insert_System_bulletin(bul_title, bul_context)
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
